eedcd1637a75de302e9bb326c1e541fece66d2ed
Erik Wolfschläger <e.wolfschlaeger@gmail.com>
2026-06-20 18:21:36 +0200

    Added plugin load

---
+++ PluginLoad.cs
@@ -0,0 +1,68 @@
+﻿using Pandemonium.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectPipeline
+{
+    internal class PluginLoad
+    {
+        public static IEnumerable<IEffectSearch> LoadEffectSearches(ILog log)
+        {
+            if (!Directory.Exists("./plugins"))
+            {
+                Directory.CreateDirectory("./plugins");
+            }
+                foreach(var plugin in Directory.EnumerateFiles("./plugins"))
+                {
+
+                    foreach(var effect_search in GetEffects(plugin, log))
+                    {
+                        yield return effect_search;
+                    }
+                }
+            foreach (var effect_search in GetEffects(typeof(Program).Assembly, log))
+            {
+                yield return effect_search;
+            }
+        }
+
+        public static IEnumerable<IEffectSearch> GetEffects(Assembly assembly, ILog log)
+        {
+            foreach (System.Type type in assembly.GetTypes().Where(type => type.IsAssignableTo(typeof(IEffectSearch)) && !(type.IsAbstract || type.IsInterface)))
+            {
+                IEffectSearch? instance = null;
+                try
+                {
+                    instance = (IEffectSearch?)Activator.CreateInstance(type);
+                }
+                catch (Exception ex)
+                {
+                    log.Warn($"Couldn't create instance of `{type.AssemblyQualifiedName ?? type.FullName ?? type.Name}`: {ex}");
+                }
+                if (instance != null)
+                    yield return instance;
+            }
+        }
+
+        public static IEnumerable<IEffectSearch> GetEffects(string file, ILog log)
+        {
+            Assembly assembly;
+            try
+            {
+                assembly = Assembly.LoadFile(file);
+
+            } catch(Exception ex)
+            {
+                log.Error($"Couldn't load assembly: {ex}");
+                return [];
+            }
+
+           return GetEffects(assembly, log);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿using Pandemonium.Engine;
+﻿using EffectPipeline.effects;
+using EffectPipeline.Effects;
+using Pandemonium.Engine;
 using Pandemonium.Engine.GameSceneStuff;
+using System.Collections.Immutable;
 
 namespace EffectPipeline
 {
     internal class Program : Game
     {
+        public static IReadOnlyList<IEffectSearch> DefaultEffectSearch { get; private set; } = null!;
+
         static void Main(string[] args)
         {
             using (var game = new Program())
@@ -18,6 +23,8 @@ namespace EffectPipeline
         protected override bool BlurryScaling => true;
         protected override void Init()
         {
+            DefaultEffectSearch = PluginLoad.LoadEffectSearches(defaultLogger).ToImmutableList();
+
             AddFontUpload("std", new FileDataUpload("./assets/fonts"));
             AddTextureUpload("std", new FileDataUpload("./assets/textures"));
         }
